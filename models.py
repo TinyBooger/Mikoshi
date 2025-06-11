9df0fbfed3f10eb26f477aafab6bf0f9ef4a108d
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-06-11 15:12:14 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Integer, JSON
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import ARRAY
 
 Base = declarative_base()
 
