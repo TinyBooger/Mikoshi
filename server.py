55bbd9cb54e7056f7dbf24802653273a24211978
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-06-23 15:13:42 +0800

    Update server.py

--- server.py
+++ server.py
@@ -208,7 +208,7 @@ def get_character(character_id: int, db: Session = Depends(get_db)):
 # -----------edit character-------------
 @app.get("/edit-character")
 async def edit_character_page():
-    return FileResponse("static/character_.html")
+    return FileResponse("static/character_edit.html")
 
 @app.post("/api/update-character")
 async def update_character(
