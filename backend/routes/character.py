a45527aba96ed3c47a03d3abe9dd1523f291e654
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-02 12:11:04 +0800

    check id match

--- backend/routes/character.py
+++ backend/routes/character.py
@@ -154,6 +154,8 @@ async def delete_character(character_id: int, request: Request, db: Session = De
     if not char:
         raise HTTPException(status_code=404, detail="Character not found")
     if char.creator_id != user.id:
+        print("creator id: ", char.creaetor_id)
+        print("user id:", user.id)
         raise HTTPException(status_code=403, detail="Not authorized")
 
     # Remove from user's characters_created list
