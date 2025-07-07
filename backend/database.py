bee9cdfd22a6af1d084514a473b974074c6e1d64
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-07 11:34:37 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://characterdb_user:kqkAuypa3xeMpNn73OkjLjfHep632yPy@dpg-d10f6eq4d50c73anu130-a.oregon-postgres.render.com/characterdb")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
