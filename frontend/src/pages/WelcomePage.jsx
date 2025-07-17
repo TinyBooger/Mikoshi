64311a4e07d496758ebb6ef03e0675c5675a2cfa
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-17 12:42:47 +0800

    Update WelcomePage.jsx

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { useNavigate } from 'react-router';
 import { authReady, secureSignInWithEmailAndPassword } from '../firebase';
 
