ffc69d7bd6b11157d98e4eacd44d603598ce93df
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-17 17:56:19 +0800

    Revert "Update firebase.js"
    This reverts commit 80d547c48e2d9004bc04d4d6777b1fcc968f8c82.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -17,8 +17,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-console.log("Firebase Config:", firebaseConfig);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
