3a4eb1303f3fa75a6e71ced12164a6bdac429327
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-17 17:55:16 +0800

    Revert "Update firebase.js"
    This reverts commit 8fe9cfa104fc3f50ff477524d1b5de0da71f9023.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_RdbZ2ENV8FEcfpkImoMUARFe1jA1rtQ",
-  authDomain: "mikoshi-135c9.firebaseapp.com",
-  projectId: "mikoshi-135c9",
-  storageBucket: "mikoshi-135c9.firebasestorage.app",
-  messagingSenderId: "39316232518",
-  appId: "1:39316232518:web:01ca7986f0a593b4e4ad47",
-  measurementId: "G-STTN82Q0FG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
