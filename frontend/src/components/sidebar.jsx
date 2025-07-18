c54781568d66515a67323650b835cf2627e8d258
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-18 15:33:50 +0800

    Update sidebar.jsx

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
-import { AuthContext } from 'AuthProvider.jsx'; // Import the AuthContext
+import { AuthContext } from './AuthProvider.jsx'; // Import the AuthContext
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 import defaultPicture from '../assets/images/default-picture.png';
