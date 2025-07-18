ca893a3def766478e9ce6990b9124ae949350c8d
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-18 10:48:25 +0800

    relative path

--- frontend/src/pages/AccountSetupPage.jsx
+++ frontend/src/pages/AccountSetupPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { auth } from './firebase';
+import { auth } from '../firebase';
 
 export default function AccountSetupPage() {
   const navigate = useNavigate();
--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from './firebase';
+import { auth } from '../firebase';
 
 export default function LoginPage() {
   const navigate = useNavigate();
