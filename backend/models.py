75043054cdc9243b7464f49814bfe821350f134b
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-07-18 14:47:46 +0800

    Update id datatype

--- backend/models.py
+++ backend/models.py
@@ -25,7 +25,7 @@ class Character(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
     name = Column(String)
     profile_pic = Column(String, nullable=True)
