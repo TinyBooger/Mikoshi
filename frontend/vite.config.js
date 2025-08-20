345451a51e6c7a83028c723ae088d8791cc5551d
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-08-20 18:00:12 +0800

    Revert "Update vite.config.js"
    This reverts commit 6eb7d8ebf3d2fbb39236f14274b4c7c0502cd565.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 // frontend/vite.config.js
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => {
   // Load environment variables for the current mode
