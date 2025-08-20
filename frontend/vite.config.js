6eb7d8ebf3d2fbb39236f14274b4c7c0502cd565
TinyBooger <66405483+TinyBooger@users.noreply.github.com>
2025-08-20 17:11:31 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 // frontend/vite.config.js
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => {
   // Load environment variables for the current mode
